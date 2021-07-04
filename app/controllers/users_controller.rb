6bc20966e42f160af2f6cfde03c5e8374a350852
EC2 Default User <ec2-user@ip-172-31-89-15.ec2.internal>
2021-07-04 10:51:35 +0000

    [フォロー機能実装途中]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
     
+    before_action :authenticate_user!
+    before_action :ensure_correct_user, only: [:edit,:update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy, :followings, :followers]
+    
     def edit
          @user = User.find(params[:id])
         if @user != current_user
@@ -29,11 +33,24 @@ class UsersController < ApplicationController
         @books = @user.books
 
     end
+    
+    def followeds
+        @followeds = @user.followed_users
+    end
+
+    def followers
+        @followers = @user.follower_users
+    end
 
     private
 
     def user_params
         params.require(:user).permit(:name, :introduction, :profile_image)
     end
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,24 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
-  
-  
+
+
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
    resource  :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:index, :show, :edit, :update]
+
+  scope '(:locale)' do
+    resources :books
+    resources :users, except: :create do
+      member do
+        get :followeds, :followers
+      end
+    end
+    resource :user_icons, only: :destroy
+    resources :relationships, only: [:create, :destroy]
+  end
 
 end
