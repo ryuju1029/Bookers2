76d36ffb5edc0480b3401e7a3295985f0afc554e
EC2 Default User <ec2-user@ip-172-31-89-15.ec2.internal>
2021-07-06 00:19:17 +0000

    [検索機能実装]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'search/search'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search' => 'search#search'
 
 
 
